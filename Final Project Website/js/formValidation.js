756f9f7474583ff09aeb5a63fc4be630ccd60742
Giorgi Tsereteli <gtsereteli@intermedia.com>
2023-05-25 20:39:24 +0400

    changed html and css

---
+++ Final Project Website/js/formValidation.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert('All fields are required!');
+        return false;
+    }
+
+    return true;
+}
